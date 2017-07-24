7f255610b9af92119a65d2edd2e8fb3552412d7e
Amine195 <gmadhgs@gmail.com>
2017-07-24 11:52:50 +0100

    Custom validate

--- .tmp/public/js/dependencies/customValidate.js
+++ .tmp/public/js/dependencies/customValidate.js
@@ -24,10 +24,14 @@ $(document).ready(function(){
 	      	equalTo: "#password"
 	      }
 	    },
-			success: function(element) {
-				element
-				.text('OK!').addClass('valid')
-			}
+      errorElement : 'div',
+      errorPlacement: function(error, element) {
+        var placement = $(element).data('error');
+        if (placement) {
+          $(placement).append(error)
+        } else {
+          error.insertAfter(element);
+        }
+      }
 	  });
-
 });
